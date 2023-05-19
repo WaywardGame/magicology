4ae2e83650d9009aa445fa9049d4f38e1206e02c
Spacetech <spacetech326@gmail.com>
2023-05-19 05:05:29 -0700

    Update for 2.13.2-beta.dev.20230519.1

--- out/IMagicology.js
+++ out/IMagicology.js
@@ -16,6 +16,6 @@ define(["require", "exports"], function (require, exports) {
     (function (MagicologyTranslation) {
         MagicologyTranslation[MagicologyTranslation["GameStatsStatManaTooltip"] = 0] = "GameStatsStatManaTooltip";
         MagicologyTranslation[MagicologyTranslation["ActionYouShootMagicalAttack"] = 1] = "ActionYouShootMagicalAttack";
-    })(MagicologyTranslation = exports.MagicologyTranslation || (exports.MagicologyTranslation = {}));
+    })(MagicologyTranslation || (exports.MagicologyTranslation = MagicologyTranslation = {}));
 });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiSU1hZ2ljb2xvZ3kuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvSU1hZ2ljb2xvZ3kudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7Ozs7Ozs7OztHQVNHOzs7OztJQUVILElBQVkscUJBR1g7SUFIRCxXQUFZLHFCQUFxQjtRQUM3Qix5R0FBd0IsQ0FBQTtRQUN4QiwrR0FBMkIsQ0FBQTtJQUMvQixDQUFDLEVBSFcscUJBQXFCLEdBQXJCLDZCQUFxQixLQUFyQiw2QkFBcUIsUUFHaEMifQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiSU1hZ2ljb2xvZ3kuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvSU1hZ2ljb2xvZ3kudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7Ozs7Ozs7OztHQVNHOzs7OztJQUVILElBQVkscUJBR1g7SUFIRCxXQUFZLHFCQUFxQjtRQUM3Qix5R0FBd0IsQ0FBQTtRQUN4QiwrR0FBMkIsQ0FBQTtJQUMvQixDQUFDLEVBSFcscUJBQXFCLHFDQUFyQixxQkFBcUIsUUFHaEMifQ==
